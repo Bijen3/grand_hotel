2f745a1dab08936e633aafeb48341455eb6b7b6a
Bijen Byanjankar <163159564+Bijen3@users.noreply.github.com>
2025-03-08 11:19:07 +0545

    Add files via upload

---
+++ db/grandhotel new.sql
@@ -0,0 +1,608 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 08, 2025 at 06:31 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `grandhotel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_cred`
+--
+
+CREATE TABLE `admin_cred` (
+  `sr_no` int(11) NOT NULL,
+  `admin_name` varchar(150) NOT NULL,
+  `admin_pass` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin_cred`
+--
+
+INSERT INTO `admin_cred` (`sr_no`, `admin_name`, `admin_pass`) VALUES
+(1, 'admin', '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `room_id` int(11) NOT NULL,
+  `check_in_date` date NOT NULL,
+  `check_out_date` date NOT NULL,
+  `total_price` decimal(10,2) NOT NULL,
+  `status` enum('pending','confirmed','cancelled') DEFAULT 'pending',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `bookings`
+--
+
+INSERT INTO `bookings` (`id`, `user_id`, `room_id`, `check_in_date`, `check_out_date`, `total_price`, `status`, `created_at`, `updated_at`) VALUES
+(9, 5, 3, '2025-03-08', '2025-03-15', 8400.00, 'cancelled', '2025-03-07 10:13:24', '2025-03-08 05:04:38'),
+(12, 5, 3, '2025-03-28', '2025-03-31', 3600.00, 'confirmed', '2025-03-08 05:01:55', '2025-03-08 05:15:21'),
+(13, 5, 1, '2025-03-15', '2025-03-19', 2000.00, 'pending', '2025-03-08 05:04:27', '2025-03-08 05:04:27'),
+(14, 5, 4, '2025-03-13', '2025-03-19', 9000.00, 'pending', '2025-03-08 05:14:30', '2025-03-08 05:14:30'),
+(15, 8, 4, '2025-03-14', '2025-03-21', 10500.00, 'pending', '2025-03-08 05:19:19', '2025-03-08 05:19:19'),
+(16, 8, 2, '2025-03-12', '2025-03-15', 3000.00, 'pending', '2025-03-08 05:23:30', '2025-03-08 05:23:30');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `carousel`
+--
+
+CREATE TABLE `carousel` (
+  `sr_no` int(11) NOT NULL,
+  `image` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `carousel`
+--
+
+INSERT INTO `carousel` (`sr_no`, `image`) VALUES
+(7, 'IMG_87930.png'),
+(8, 'IMG_59158.png'),
+(9, 'IMG_42210.png'),
+(10, 'IMG_33908.png'),
+(11, 'IMG_49599.png'),
+(12, 'IMG_90477.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_details`
+--
+
+CREATE TABLE `contact_details` (
+  `sr_no` int(11) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `gmap` varchar(100) NOT NULL,
+  `pn1` varchar(30) NOT NULL,
+  `pn2` varchar(30) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `fb` varchar(100) NOT NULL,
+  `insta` varchar(100) NOT NULL,
+  `tw` varchar(100) NOT NULL,
+  `iframe` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contact_details`
+--
+
+INSERT INTO `contact_details` (`sr_no`, `address`, `gmap`, `pn1`, `pn2`, `email`, `fb`, `insta`, `tw`, `iframe`) VALUES
+(1, 'XYZ, Lalitpur, Nepal', 'https://maps.app.goo.gl/M7qd7VvJYSMzGF7p7', '9779823526712', '9779812361525', 'grandhotel88@gmail.com', 'https://www.facebook.com/', 'https://www.instagram.com/', 'https://x.com/', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d56542.96717960321!2d85.32315195000001!3d27.6574688!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19d3cf18ca51:0xd10ec3d53656e18f!2sLalitpur!5e0!3m2!1sen!2snp!4v1740662545427!5m2!1sen!2snp');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`id`, `name`, `email`, `password`, `created_at`) VALUES
+(4, 'bizen', 'bizen@gmail.com', '$2y$10$mA.mABLuHMu2Cpb/8KKbueyzyZ5vXkXp1tf2nrNdG9weZ/lve7j3S', '2025-03-07 08:38:36'),
+(5, 'bijen', 'bijen@gmail.com', '$2y$10$Q9z4xClBRa7E1ytkrPcHsOzx1Zm4HgJ55HJ7729HbXu8juR0Q4pae', '2025-03-07 08:45:03'),
+(6, 'zen', 'zen@gmail.com', '$2y$10$XM7JeHkXJeD1Sb68//EzP.DdR/jt/e5ZP11Sfiz0ySuQm5ZBXoisO', '2025-03-07 08:50:03'),
+(7, 'test', 'test@gmail.com', '$2y$10$.bGfDq.SmYul.NIgkGL0i.JYZG6Bv2DKMzuG7N/b9nYs0fjh5m1Fu', '2025-03-07 09:33:33'),
+(8, 'dell', 'dell@gmail.com', '$2y$10$rcKQQxN6v.Ox6sgcZFysUurdxf2A1ucbYb6rF9wsA/8zXz59ddD4S', '2025-03-07 09:34:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facilities`
+--
+
+CREATE TABLE `facilities` (
+  `id` int(11) NOT NULL,
+  `icon` varchar(100) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `facilities`
+--
+
+INSERT INTO `facilities` (`id`, `icon`, `name`, `description`) VALUES
+(7, 'IMG_93196.svg', 'Air Conditioner', 'A cooling device, usually a wall-mounted or window unit, that is installed within a single room to regulate its temperature by drawing in warm air, cooling it down through a refrigerant process, and then circulating the chilled air back into the room'),
+(8, 'IMG_43022.svg', 'Geyser', 'A water heating appliance, typically mounted on a bathroom wall, that provides hot water on demand for showering or washing, essentially a &quot;water heater&quot; that heats water quickly using an electric heating element.'),
+(9, 'IMG_59855.svg', 'Heater', 'Room heaters are used to heat a small space and are normally portable or fitted to a wall. Most room heaters use gas or electricity.'),
+(10, 'IMG_61084.svg', 'Television', 'A flat-screen television is mounted on the wall or sitting on a stand within a particular room, often mentioning its size and potential viewing position relative to seating in the area'),
+(11, 'IMG_47596.svg', 'Spa', 'A spa is a location where mineral-rich spring water is used to give medicinal baths. Spa health treatments are known as balneotherapy. The belief in the curative powers of mineral waters and hot springs goes back to prehistoric times.'),
+(12, 'IMG_37919.svg', 'Wifi', 'It delivers dedicated guest room WiFi relies on the network&#039;s infrastructure of routers, access points, and switches.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `features`
+--
+
+CREATE TABLE `features` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `features`
+--
+
+INSERT INTO `features` (`id`, `name`) VALUES
+(1, 'bedroom'),
+(2, 'kitchen'),
+(3, 'table'),
+(5, 'dining'),
+(6, 'sofas');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `area` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `adult` int(11) NOT NULL,
+  `children` int(11) NOT NULL,
+  `description` varchar(350) NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  `removed` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `rooms`
+--
+
+INSERT INTO `rooms` (`id`, `name`, `area`, `price`, `quantity`, `adult`, `children`, `description`, `status`, `removed`) VALUES
+(1, 'Simple Room', 200, 500, 10, 5, 3, 'A standard, basic room with a single bed, attached bathroom, and essential amenities like a TV.', 1, 0),
+(2, 'Deluxe Room', 300, 1000, 12, 6, 3, 'It has more space, upgraded amenities, high-quality furnishings, and often a better view.', 1, 0),
+(3, 'Premium Room', 500, 1200, 6, 8, 5, 'It offers more space, better amenities, and a more luxurious experience with different luxurious features and facilities.', 1, 0),
+(4, 'Super Room', 250, 1500, 15, 7, 2, 'A hotel room that is significantly larger and more luxurious than a standard room, offering upgraded spacious layout and other enhanced facilities.', 1, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room_facilities`
+--
+
+CREATE TABLE `room_facilities` (
+  `sr_no` int(11) NOT NULL,
+  `room_id` int(11) NOT NULL,
+  `facilities_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `room_facilities`
+--
+
+INSERT INTO `room_facilities` (`sr_no`, `room_id`, `facilities_id`) VALUES
+(29, 1, 9),
+(30, 1, 10),
+(31, 1, 12),
+(32, 2, 7),
+(33, 2, 8),
+(34, 2, 9),
+(35, 2, 12),
+(40, 4, 7),
+(41, 4, 8),
+(42, 4, 10),
+(43, 4, 12),
+(44, 3, 7),
+(45, 3, 8),
+(46, 3, 9),
+(47, 3, 11),
+(48, 3, 12);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room_features`
+--
+
+CREATE TABLE `room_features` (
+  `sr_no` int(11) NOT NULL,
+  `room_id` int(11) NOT NULL,
+  `features_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `room_features`
+--
+
+INSERT INTO `room_features` (`sr_no`, `room_id`, `features_id`) VALUES
+(16, 1, 1),
+(17, 1, 3),
+(18, 1, 6),
+(19, 2, 1),
+(20, 2, 2),
+(21, 2, 3),
+(22, 2, 5),
+(27, 4, 1),
+(28, 4, 3),
+(29, 4, 6),
+(30, 3, 1),
+(31, 3, 2),
+(32, 3, 5),
+(33, 3, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room_images`
+--
+
+CREATE TABLE `room_images` (
+  `sr_no` int(11) NOT NULL,
+  `room_id` int(11) NOT NULL,
+  `image` varchar(150) NOT NULL,
+  `thumb` tinyint(4) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `room_images`
+--
+
+INSERT INTO `room_images` (`sr_no`, `room_id`, `image`, `thumb`) VALUES
+(13, 1, 'IMG_65559.jpg', 0),
+(14, 1, 'IMG_42285.png', 0),
+(15, 1, 'IMG_93242.png', 1),
+(16, 2, 'IMG_93767.png', 0),
+(17, 2, 'IMG_58337.png', 1),
+(18, 2, 'IMG_83300.png', 0),
+(19, 3, 'IMG_19613.png', 0),
+(20, 3, 'IMG_13616.png', 1),
+(21, 3, 'IMG_37138.png', 0),
+(22, 4, 'IMG_71761.png', 1),
+(23, 4, 'IMG_96397.png', 0),
+(24, 4, 'IMG_56941.png', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE `settings` (
+  `sr_no` int(11) NOT NULL,
+  `site_title` varchar(50) NOT NULL,
+  `site_about` varchar(550) NOT NULL,
+  `shutdown` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`sr_no`, `site_title`, `site_about`, `shutdown`) VALUES
+(1, 'GRAND HOTEL', 'Escape to comfort and luxury at GRAND HOTEL, where relaxation meets impeccable service. Nestled in the heart of Lalitpur, our hotel offers the perfect blend of modern amenities and cozy accommodations. Whether you are here for business or leisure, you will enjoy spacious rooms, world-class dining, and a range of exclusive amenities. Let us make your stay unforgettable with personalized service and a warm, welcoming atmosphere. Book your stay today and experience the ultimate in comfort and convenience.', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `team_details`
+--
+
+CREATE TABLE `team_details` (
+  `sr_no` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `picture` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `team_details`
+--
+
+INSERT INTO `team_details` (`sr_no`, `name`, `picture`) VALUES
+(13, 'one', 'IMG_46852.jpg'),
+(14, 'two', 'IMG_44272.jpg'),
+(15, 'three', 'IMG_17922.jpg'),
+(16, 'four', 'IMG_37343.jpg'),
+(17, 'five', 'IMG_79630.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_queries`
+--
+
+CREATE TABLE `user_queries` (
+  `sr_no` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `subject` varchar(200) NOT NULL,
+  `message` varchar(500) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp(),
+  `seen` tinyint(4) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_queries`
+--
+
+INSERT INTO `user_queries` (`sr_no`, `name`, `email`, `subject`, `message`, `date`, `seen`) VALUES
+(14, 'Ram Bahadur', 'rambdr@gmail.com', 'facilities', 'There are lots of facilities that are required for the room.', '2025-02-28', 0),
+(15, 'Zen Tandukar', 'zen@gmail.com', 'features', 'There are lots of features that are required for the room.', '2025-02-28', 0),
+(16, 'Zeff Benz', 'zeff@gmail.com', 'services', 'This hotel provides the best services.', '2025-02-28', 0),
+(17, 'Roger Lama', 'rogerlm@gmail.com', 'rooms', 'All the rooms are well managed.', '2025-02-28', 0),
+(18, 'Praz Thapa', 'praz@gmail.com', 'cleanliness', 'Everything is cleaned and properly kept.', '2025-02-28', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_cred`
+--
+ALTER TABLE `admin_cred`
+  ADD PRIMARY KEY (`sr_no`);
+
+--
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `room_id` (`room_id`);
+
+--
+-- Indexes for table `carousel`
+--
+ALTER TABLE `carousel`
+  ADD PRIMARY KEY (`sr_no`);
+
+--
+-- Indexes for table `contact_details`
+--
+ALTER TABLE `contact_details`
+  ADD PRIMARY KEY (`sr_no`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `facilities`
+--
+ALTER TABLE `facilities`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `features`
+--
+ALTER TABLE `features`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `room_facilities`
+--
+ALTER TABLE `room_facilities`
+  ADD PRIMARY KEY (`sr_no`),
+  ADD KEY `room id` (`room_id`),
+  ADD KEY `facilities id` (`facilities_id`);
+
+--
+-- Indexes for table `room_features`
+--
+ALTER TABLE `room_features`
+  ADD PRIMARY KEY (`sr_no`),
+  ADD KEY `features id` (`features_id`),
+  ADD KEY `rm id` (`room_id`);
+
+--
+-- Indexes for table `room_images`
+--
+ALTER TABLE `room_images`
+  ADD PRIMARY KEY (`sr_no`),
+  ADD KEY `room_id` (`room_id`);
+
+--
+-- Indexes for table `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`sr_no`);
+
+--
+-- Indexes for table `team_details`
+--
+ALTER TABLE `team_details`
+  ADD PRIMARY KEY (`sr_no`);
+
+--
+-- Indexes for table `user_queries`
+--
+ALTER TABLE `user_queries`
+  ADD PRIMARY KEY (`sr_no`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_cred`
+--
+ALTER TABLE `admin_cred`
+  MODIFY `sr_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `carousel`
+--
+ALTER TABLE `carousel`
+  MODIFY `sr_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `contact_details`
+--
+ALTER TABLE `contact_details`
+  MODIFY `sr_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `facilities`
+--
+ALTER TABLE `facilities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `features`
+--
+ALTER TABLE `features`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `room_facilities`
+--
+ALTER TABLE `room_facilities`
+  MODIFY `sr_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+
+--
+-- AUTO_INCREMENT for table `room_features`
+--
+ALTER TABLE `room_features`
+  MODIFY `sr_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT for table `room_images`
+--
+ALTER TABLE `room_images`
+  MODIFY `sr_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `settings`
+--
+ALTER TABLE `settings`
+  MODIFY `sr_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `team_details`
+--
+ALTER TABLE `team_details`
+  MODIFY `sr_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `user_queries`
+--
+ALTER TABLE `user_queries`
+  MODIFY `sr_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `customer` (`id`),
+  ADD CONSTRAINT `bookings_ibfk_2` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`);
+
+--
+-- Constraints for table `room_facilities`
+--
+ALTER TABLE `room_facilities`
+  ADD CONSTRAINT `facilities id` FOREIGN KEY (`facilities_id`) REFERENCES `facilities` (`id`) ON UPDATE NO ACTION,
+  ADD CONSTRAINT `room id` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`) ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `room_features`
+--
+ALTER TABLE `room_features`
+  ADD CONSTRAINT `features id` FOREIGN KEY (`features_id`) REFERENCES `features` (`id`) ON UPDATE NO ACTION,
+  ADD CONSTRAINT `rm id` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`) ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `room_images`
+--
+ALTER TABLE `room_images`
+  ADD CONSTRAINT `room_images_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
